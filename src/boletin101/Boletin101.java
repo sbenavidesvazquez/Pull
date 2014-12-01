7f8ea3917ab4fb8d262f4cc47e24807f996b5149
sbenavidesvazquez <sbenavidesvazquez@eniac22.danielcastelao.org>
2014-12-01 09:17:42 +0100

    juego sin intentos

---
+++ src/boletin101/Boletin101.java
@@ -0,0 +1,19 @@
+
+package boletin101;
+
+
+public class Boletin101 {
+
+   
+    public static void main(String[] args) {
+        
+        juego obx=new juego();
+        obx.opciones();
+        obx.numAle();
+        obx.empezar();
+        
+    
+        
+    }
+    
+}
--- src/boletin101/juego.java
+++ src/boletin101/juego.java
@@ -7,7 +7,17 @@ import javax.swing.JOptionPane;
 public class juego {
     
     String a;
-    int n, s;
+    int n, s, d;
+    
+    
+    public int opciones(){
+        
+        do{
+        a=JOptionPane.showInputDialog("Escoje los turnos entre 5 y 20");
+        d=Integer.parseInt(a);
+        }while(d<5 || d>20);
+        return d;
+    }
     
     public void numAle(){
         
@@ -17,23 +27,46 @@ public class juego {
         n=Integer.parseInt(a);
         
         }while(n<1 || n>50);
-         do{
+         
+        
+    }
+    
+    public void empezar(){
+        
+        int i=1;
+        
+         for(int i=1;i<=d;i++){
+         
+        
+            
             a=JOptionPane.showInputDialog("JUGADOR 2, Introduzca su respuesta");
             s=Integer.parseInt(a);
             if(s<n){
                 JOptionPane.showMessageDialog(null, "el número es mayor");
+                i=i+1;                
             }
             else if(s>n){
                 JOptionPane.showMessageDialog(null, "El número es menor");
+                i=i+1;
             }
-            
-        }while(s!=n);
+           }  
+         
         JOptionPane.showMessageDialog(null, "GANASTE");
+    
+         
+                JOptionPane.showMessageDialog(null, "PERDISTE");
+        }
         
-    }
+           
+          } 
+                         
     
     
+        
     
     
+        
     
-}
+
+
+
